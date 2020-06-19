226a4ce5bbd9cacdf4e583f7e9955be7cd0018d4
tirth97 <patel.tir@husky.neu.edu>
2020-06-19 00:39:26 -0400

    Adding a basic Registration Page.

---
+++ conference/src/main/java/com/pluralsight/conference/controller/RegistrationController.java
@@ -0,0 +1,15 @@
+package com.pluralsight.conference.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.Map;
+
+@Controller
+public class RegistrationController {
+
+  @GetMapping("registration")
+  public String getRegistration(Map<String, Object> model) {
+    return "registration";
+  }
+}
---
+++ conference/src/main/webapp/WEB-INF/jsp/registration.jsp
@@ -0,0 +1,16 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: SPAdmin
+  Date: 6/19/2020
+  Time: 12:21 AM
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+<head>
+    <title>Registration</title>
+</head>
+<body>
+    <h1>In Registration Page</h1>
+</body>
+</html>
